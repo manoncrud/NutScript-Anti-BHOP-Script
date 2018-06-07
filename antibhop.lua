b6e653602ae5bdc76b4fdd28b18d7e4f812ab0a0
Pingersssss <T.Smith-Stanley@larkmead.oxon.sch.uk>
2018-06-07 18:45:35 +0100

    Create antibhop.lua

---
+++ antibhop.lua
@@ -0,0 +1,17 @@
+PLUGIN.name = "Anti-Bunny Hop"
+PLUGIN.author = "Pingers"
+PLUGIN.desc = "Stop people from constantly jumping on servers"
+--This time, ego free!
+
+hook.Add( "CreateMove", "Stamina:DisableJumpKey", function( input )
+	if ( LocalPlayer( ):IsAdmin( ) ) then 
+		return end
+	if ( input:KeyDown( IN_JUMP ) ) then
+		if ( LocalPlayer( ):GetJumpPower( ) < PLAYER_JUMP_POWER_MIN ) then 
+			local _buttons = input:GetButtons();
+			input:SetButtons( _buttons - IN_JUMP )
+		end
+	end
+end )
+
+--[[ https://team.paragonservers.com/pingers--]] 
